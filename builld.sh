8575afb4d034cb271ce2f20cc21ebb059ba6e35d
root <root@localhost>
2018-03-18 10:02:31 +0900

    build, tag script

--- builld.sh
+++ builld.sh
@@ -1,4 +1,9 @@
 ./update.sh
+docker build -t buxis/redmine:3.2.9 3.2
+docker build -t buxis/redmine:3.2.9-passenger 3.2/passenger
+
 docker build -t buxis/redmine:3.3.6 3.3
-docker tag buxis/redmine:3.3.6 buxis/redmine:3.3
 docker build -t buxis/redmine:3.3.6-passenger 3.3/passenger
+
+docker build -t buxis/redmine:3.4.4 3.4
+docker build -t buxis/redmine:3.4.4-passenger 3.4/passenger
---
+++ tag.sh
@@ -0,0 +1,11 @@
+docker tag buxis/redmine:3.2.9 buxis/redmine:3.2
+docker tag buxis/redmine:3.2.9-passenger buxis/redmine:3.2-passenger
+
+docker tag buxis/redmine:3.3.6 buxis/redmine:3.3
+docker tag buxis/redmine:3.3.6-passenger buxis/redmine:3.3-passenger
+
+docker tag buxis/redmine:3.4.4 buxis/redmine:3.4
+docker tag buxis/redmine:3.4.4 buxis/redmine:3
+docker tag buxis/redmine:3.4.4 buxis/redmine:latest
+docker tag buxis/redmine:3.4.4-passenger buxis/redmine:3.4-passenger
+docker tag buxis/redmine:3.4.4-passenger buxis/redmine:3-passenger
